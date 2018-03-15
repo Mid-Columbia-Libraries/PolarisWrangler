bddbff4d133efe5fc9d8720144ae61aa92315f3b
Vance <vcole@midcolumbialibraries.org>
2018-03-15 15:28:50 -0700

    implements xml/json switch, API key toggle to API Wrangler

--- src/pages/wrangler-api.vue
+++ src/pages/wrangler-api.vue
@@ -1,25 +1,50 @@
 <template>
-  <q-page class="flex items-start column full-width ">
-    <div class="searchbar full-width row q-pa-md">
-      <div class="col-1" />
+  <q-page class="flex items-start column full-width">
+    <div class="searchbar justify-center full-width row q-pa-sm">
       <q-input
-        class="col-7 q-ml-sm"
+        class="full-width"
         v-model="query"
         inverted
         placeholder="Enter a query"
         autofocus
         clearable
       />
+    </div>
+    <div class="full-width row justify-center q-pl-sm q-pr-sm q-pb-sm">
+      <q-select
+        class="col-2 q-ml-sm"
+        v-model="authlvl"
+        :options="authOptions"
+        inverted
+      />
+      <q-checkbox class="col-2 round-borders text-dark bg-white q-ml-sm q-pa-sm"
+        v-model="useToken" label="Use Credentials" />
+      <q-input
+        class="col-2 q-ml-sm"
+        v-model="user"
+        inverted
+        placeholder="User"
+        clearable
+        :disabled="!useToken"
+      />
+      <q-input
+        class="col-2 q-ml-sm"
+        v-model="pass"
+        inverted
+        placeholder="Password"
+        clearable
+        :disabled="!useToken"
+      />
       <q-select
         class="col-1 q-ml-sm"
-        v-model="auth"
-        :options="selectOptions"
+        v-model="params.accept"
+        :options="responseOptions"
         inverted
       />
       <q-btn
         @click.native="wrangle"
         v-if="!status"
-        class="bg-positive col-2 q-ml-sm"
+        class="col-2 bg-positive q-ml-sm"
         text-color="tertiary"
         label="Wrangle"
         icon="healing"
@@ -28,16 +53,18 @@
       <q-btn
         @click.native="abort"
         v-if="status"
-        class="bg-negative col-2 q-ml-sm"
+        class="col-2 bg-negative q-ml-sm"
         text-color="tertiary"
         label="Abort!"
         icon="report"
         flat
       />
-      <div class="col-1" />
     </div>
     <div class="output bg-tertiary full-width q-pa-md">
-      <vue-json-pretty :data="data" />
+      <vue-json-pretty :data="output.data" />
+    </div>
+    <div class="config bg-tertiary full-width q-mt-md q-pa-md">
+      <vue-json-pretty :data="output.config" />
     </div>
   </q-page>
 </template>
@@ -48,25 +75,49 @@ import VueJsonPretty from 'vue-json-pretty';
 export default {
   data() {
     return {
+      // The query string user input
       query: '',
-      auth: false,
+      // API call paramss generated from input
+      params: {
+        accept: 'application/json',
+      },
+      // Whether to use Patron or Protected authlvl
+      authlvl: 'protected',
+      useToken: false,
+      token: false,
+      user: '',
+      pass: '',
       status: false,
-      selectOptions: [
+      authOptions: [
         {
-          label: 'Public',
-          value: false,
+          label: 'No API Key',
+          value: 'patron',
         },
         {
-          label: 'Protected',
-          value: true,
+          label: 'Use API Key',
+          value: 'protected',
+        },
+      ],
+      responseOptions: [
+        {
+          label: 'JSON',
+          value: 'application/json',
+        },
+        {
+          label: 'XML',
+          value: 'application/xml',
         },
       ],
       api: this.$jsPAPI({
         server: this.$config.get('polarisUrl'),
         accessid: this.$config.get('polarisId'),
         key: this.$config.get('polarisKey'),
+        authlvl: this.authlvl,
       }),
-      data: {},
+      output: {
+        data: {},
+        config: {},
+      },
     };
   },
   computed: {
@@ -75,18 +126,42 @@ export default {
     VueJsonPretty,
   },
   watch: {
+    useToken() {
+      if (this.useToken) this.authlvl = 'protected';
+    },
+    authlvl() {
+      this.api = this.$jsPAPI({
+        server: this.$config.get('polarisUrl'),
+        accessid: this.$config.get('polarisId'),
+        key: this.$config.get('polarisKey'),
+        authlvl: this.authlvl,
+      });
+    },
   },
   methods: {
+    getCfg(input) {
+      const cfg = input.config;
+      delete cfg.adapter;
+      delete cfg.transformRequest;
+      delete cfg.transformResponse;
+      return cfg;
+    },
     wrangle() {
       this.status = true;
-      this.api.call(this.query)
+      this.output.data = {};
+      this.output.config = {};
+      const pass = Object.assign({}, this.params);
+      this.api.call(this.query, pass)
         .then((r) => {
           this.status = false;
-          this.data = r;
+          this.output.config = this.getCfg(r);
+          delete r.config;
+          this.output.data = r;
+          this.status = false;
         })
         .catch((e) => {
+          this.output.config = this.getCfg(e);
           this.status = false;
-          this.data = e;
         });
     },
     abort() {
@@ -105,5 +180,6 @@ export default {
     margin-bottom: 24px
     z-index: 100
   .output span
+  .config span
     font-family: 'Monaco', 'Menlo', 'Consolas', 'Bitstream Vera Sans Mono', monospace
 </style>
